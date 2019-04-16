279f17deed0ef9cc978a063dc8d2e2c0dabe69bc
Terashima <yamanogomora@gmail.com>
2019-04-16 18:16:38 +0900

    Add: UIパーツをコードに紐付ける

--- JankenApp/ViewController.swift
+++ JankenApp/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var answerImageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBAction func shuffleAction(_ sender: Any) {
+    }
+    
 }
 
